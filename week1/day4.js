dd53f5c3a814c9400d409359cb27bb85744f8ca3
worknesh <workneshsun@gmail.com>
2019-06-08 10:33:56 +0300

    my day4 js

---
+++ week1/day4.js
@@ -0,0 +1,20 @@
+
+var book = {
+ title: " dertogada",
+ author: "esmake werku",
+ releaseDate: 2000 E.c,  }
+ 
+console.log(book)  
+console.log(book.title) 
+
+var food = ["vegetable", "shiro", "bread"]
+console.log(food)
+var i;
+
+for (i = 0; i<3; i++){
+  console.log (food[i])
+ }
+      var count = 1;
+    while (count <= 10 ){
+      console.log(count);
+          count += 1;
