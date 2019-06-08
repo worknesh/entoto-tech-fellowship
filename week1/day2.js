e299653166288cabf6cfb2d01942157511db9185
worknesh <workneshsun@gmail.com>
2019-06-08 10:26:16 +0300

    my day2 java

---
+++ week1/day2.js
@@ -0,0 +1,13 @@
+
+ var weatherTemperature = 15
+// console.log(weatherTemperature)
+// if ( weatherTemperature < 10 )
+// {
+//   console.log("it's cold outside")
+// }
+// else if ( weatherTemperature > 20 ){
+//   console.log("it's warm outside")
+// }
+// else {
+//   console.log("it's nice outside")
+// }
